7b3a012015d246847065882601a8eb05e28ad474
dpavon <daniti333@gmail.com>
2022-02-10 19:18:03 +0100

    info change

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dpavon-g <dpavon-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 23:56:54 by ldurante          #+#    #+#             */
-/*   Updated: 2022/02/08 23:58:24 by ldurante         ###   ########.fr       */
+/*   Updated: 2022/02/10 19:16:44 by dpavon-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,33 @@
 # include <sys/stat.h>
 # include <errno.h>
 
+typedef struct s_data
+{
+	char	*no;
+	char	*so;
+	char	*we;
+	char	*ea;
+	int		floor[3];
+	int		ceiling[3];
+	char	**map;
+}	t_data;
+
 #endif
+
+/*
+
+	Cosas a revisar:
+
+	1. Check_error:
+		- Extension del .cub
+		- Extension de las texturas.
+		- Nombre de la variable correcto en el fichero.
+		- RGB de suelo y de Cielo deben ser entre 0 y 255.
+		- Dentro del mapa:
+			· Mirar que los bordes del mapa estén correctamente posicionados.
+			· Que no haya mas de una W.
+	2.	Reescribir atoi.
+	3.	Probablemente necesitemos una estructura para almacenar datos especificos
+		del mapa.
+	4.	Separar el char_info.
+*/
--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,13 @@
 /*   By: dpavon-g <dpavon-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 00:02:49 by ldurante          #+#    #+#             */
-/*   Updated: 2022/02/10 18:24:00 by dpavon-g         ###   ########.fr       */
+/*   Updated: 2022/02/10 18:41:20 by dpavon-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../includes/cub3d.h"
 
-char	**ft_realloc(char **map, char **line)
+char	**ft_realloc(char **info, char **line)
 {
 	int		size;
 	char	**aux;
@@ -20,18 +20,18 @@ char	**ft_realloc(char **map, char **line)
 
 	i = 0;
 	aux = NULL;
-	if (!(map))
+	if (!(info))
 	{
 		aux = malloc(sizeof(char *) * 2);
 		aux[0] = *line;
 		aux[1] = NULL;
 		return (aux);
 	}
-	size = matrix_len(map);
+	size = matrix_len(info);
 	aux = malloc(sizeof(char *) * (size + 2));
 	while (i < size)
 	{
-		aux[i] = ft_strdup(map[i]);
+		aux[i] = ft_strdup(info[i]);
 		i++;
 	}	
 	aux[size] = *line;
@@ -42,39 +42,39 @@ char	**ft_realloc(char **map, char **line)
 char	**get_info(char **argv)
 {
 	int		fd;
-	char	**map;
+	char	**info;
 	char	*line;
 	char	**aux;
 	int		ret;
 
 	line = NULL;
-	map = NULL;
+	info = NULL;
 	fd = open(argv[1], O_RDONLY);
 	ret = get_next_line(fd, &line);
 	while (ret >= 0)
 	{
-		aux = ft_realloc(map, &line);
-		free_matrix(map);
-		map = matrix_dup(aux);
+		aux = ft_realloc(info, &line);
+		free_matrix(info);
+		info = matrix_dup(aux);
 		free_matrix(aux);
 		if (ret == 0)
 			break;
 		ret = get_next_line(fd, &line);
 	}
 	close(fd);
-	return (map);
+	return (info);
 }
 
 int	main(int argc, char **argv)
 {
-	char	**map;
+	char	**info;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	map = get_info(argv);
-	free_matrix(map);
+	info = get_info(argv);
+	free_matrix(info);
 	return (0);
 }
