33cd7635d4bf1a44a36b70ec42d5b5cecdf41c53
durantecode <durantecode@gmail.com>
2022-03-02 00:40:03 +0100

    wall orientation almost working, must fix some precission/rendering

--- srcs/minimap.c
+++ srcs/minimap.c
@@ -6,7 +6,7 @@
 /*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 14:52:40 by ldurante          #+#    #+#             */
-/*   Updated: 2022/03/01 18:38:06 by ldurante         ###   ########.fr       */
+/*   Updated: 2022/03/02 00:38:09 by ldurante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,21 @@ typedef struct mini_vector
 void	draw_line(t_game *g, t_img img)
 {
 	float		r;
-	t_bres		bres;
+	t_bres		ray;
 	t_vector	vector;
 
-	ft_bzero(&bres, sizeof(t_bres));
+	ft_bzero(&ray, sizeof(t_bres));
 	vector = get_map_vector(g);
-	bres.x = MINI_MAP_CENTER + TILE_SIZE / 2;
-	bres.y = MINI_MAP_CENTER + TILE_SIZE / 2;
+	ray.x = MINI_MAP_CENTER + TILE_SIZE / 2;
+	ray.y = MINI_MAP_CENTER + TILE_SIZE / 2;
 	r = 0;
-	bres.end_x = bres.x + r * cos(g->player.dir + g->player.rotate);
-	bres.end_y = bres.y + r * sin(g->player.dir + g->player.rotate);
-	while (g->map[(vector.y + (int)bres.end_y - (int)bres.y) / TILE_SIZE]
-		[(vector.x + (int)bres.end_x - (int)bres.x) / TILE_SIZE] != '1')
+	ray.end_x = ray.x + r * cos(g->player.dir + g->player.rotate);
+	ray.end_y = ray.y + r * sin(g->player.dir + g->player.rotate);
+	while (g->map[(vector.y + (int)ray.end_y - (int)ray.y) / TILE_SIZE]
+		[(vector.x + (int)ray.end_x - (int)ray.x) / TILE_SIZE] != '1')
 	{
-		bres.end_x = bres.x + r * cos(g->player.dir + g->player.rotate);
-		bres.end_y = bres.y + r * sin(g->player.dir + g->player.rotate);
+		ray.end_x = ray.x + r * cos(g->player.dir + g->player.rotate);
+		ray.end_y = ray.y + r * sin(g->player.dir + g->player.rotate);
 		r++;
 	}
 	g->player.step_vx = cos(g->player.dir + g->player.rotate) * PLAYER_SPEED;
@@ -48,118 +48,91 @@ void	draw_line(t_game *g, t_img img)
 	(void)img;
 }
 
-int	ft_colorcmp(int y, int x, int color, t_img *img)
-{
-	char	*dst;
+// int	ft_colorcmp(int y, int x, int color, t_img *img)
+// {
+// 	char	*dst;
 
-	dst = img->addr + (y * img->line_len
-				+ x * (img->bpp / 8));
-	if (*(int *)dst == color)
-		return (1);
-	return (0);
+// 	dst = img->addr + (y * img->line_len
+// 				+ x * (img->bpp / 8));
+// 	if (*(int *)dst == color)
+// 		return (1);
+// 	return (0);
+// }
+
+int	get_texture(t_game *g, t_bres ray)
+{
+	float	small;
+	int		texture;
+
+	small = 0.1106666;
+	// small = 0.0509;
+	// small = 0.00599;
+	// small = 0.05;
+	texture = 0;
+	(void)g;
+	if (ray.end_y - (int)ray.end_y < small && \
+			ray.end_x > (int)ray.end_x + small && ray.end_x < (int)ray.end_x + 1 - small)
+		texture = WALL_PURPLE;
+	else if ((int)ray.end_y + 1 - ray.end_y < small && \
+		ray.end_x > (int)ray.end_x + small && ray.end_x < (int)ray.end_x + 1 - small)
+		texture = WALL_PURPLE_DARK;
+	else if (ray.end_x - (int)ray.end_x < small && \
+		ray.end_y > (int)ray.end_y + small && ray.end_y < (int)ray.end_y + 1 - small)
+		texture = PLAYER_RED;
+	else if ((int)ray.end_x + 1 - ray.end_x < small && \
+		ray.end_y > (int)ray.end_y + small && ray.end_y < (int)ray.end_y + 1 - small)
+		texture = FOV_BEIGE;
+	return (texture);
 }
 
-void	draw_walls(float r, float i, int column, t_game *g, t_bres bres, t_vector vector)
+void	draw_walls(float r, float i, int column, t_game *g, t_bres ray)
 {
 	float	wall_height;
 	float	distance;
 	int		texture;
 
-	texture = 0;
-
-	// if ((int)(bres.end_y - (g->player.y * TILE_SIZE - MINI_MAP_CENTER) + g->player.move_y) % 9 == 0)
-	// printf("AA: %d\n", vector.y);
-	// 	texture = WALL_PURPLE;
-	// if ((int)bres.end_y % 9 == 0)
-	// 	texture = WALL_PURPLE;
-	// if (g->map[(vector.y + (int)bres.end_y - 1 - (int)bres.y) / TILE_SIZE]		
-	// 	[(vector.x + (int)bres.end_x + 1 - (int)bres.x) / TILE_SIZE] == '1')
-	// printf("YY: %f, XX: %f\n", bres.end_y - 1, bres.end_x);
-	if (bres.end_y - (int)bres.end_y > 0.5 && bres.end_y - (int)bres.end_y < 0.9)
-	// if (ft_colorcmp(bres.end_y - 1, bres.end_x, WALL_PURPLE, &g->mini_map))
-		texture = WALL_PURPLE;
-	// if (bres.end_x - (int)bres.end_x > 0.5 && bres.end_x - (int)bres.end_x < 0.9)
-	// 	texture = WALL_PURPLE_DARK;
-	else if (bres.end_y - (int)bres.end_y < 0.5 && bres.end_y - (int)bres.end_y > 0)
-		texture = PLAYER_RED;
-	// if (bres.end_x - (int)bres.end_x < 0.5 && bres.end_x - (int)bres.end_x > 0)
-	else
-		texture = FOV_BEIGE;
-
-	// if (ft_colorcmp(bres.end_y, bres.end_x + 1, WALL_PURPLE, &g->mini_map))
-	// 	texture = 0;
-	// if (ft_colorcmp(bres.end_y + 1, bres.end_x, WALL_PURPLE, &g->mini_map))
-	// 	texture = WALL_PURPLE_DARK;
-	// if (ft_colorcmp(bres.end_y, bres.end_x + 1, WALL_PURPLE, &g->mini_map))
-	// 	texture = 0;
-	// else if (g->map[(vector.y + (int)bres.end_y - 1 - (int)bres.y) / TILE_SIZE]		
-	// 	[(vector.x + (int)bres.end_x + 1 - (int)bres.x) / TILE_SIZE] == '1')
-	// 	texture = WALL_PURPLE_DARK;
-
-		// texture = FOV_BEIGE;
-
-		// texture = PLAYER_RED;
-	(void)vector;
-	bres.x = column;
-	bres.end_x = column;
+	texture = get_texture(g, ray);
+	ray.x = column;
+	ray.end_x = column;
 	distance = r * cos(i);
 	wall_height = 4500 / distance;
-	bres.y = WIN_HALF - wall_height;
-	bres.end_y = WIN_HALF + wall_height;
-	write_line_bres(g->bg, bres, texture);
+	ray.y = WIN_HALF - wall_height;
+	ray.end_y = WIN_HALF + wall_height;
+	write_line_bres(g->bg, ray, texture);
 }
 
 void	draw_fov(t_game *g, t_img img)
 {
 	float		i;
 	float		r;
-	t_bres		bres;
+	t_bres		ray;
 	t_vector	vector;
-	int			pixel;
+	int			ray_count;
 
-	pixel = 0;
-	vector = get_map_vector(g);
+	ray_count = 0;
 	i = -FOV_ANGLE;
-	ft_bzero(&bres, sizeof(t_bres));
-	bres.x = MINI_MAP_CENTER + TILE_SIZE / 2;
-	bres.y = MINI_MAP_CENTER + TILE_SIZE / 2;
-	while (i < FOV_ANGLE)
+	vector = get_map_vector(g);
+	ft_bzero(&ray, sizeof(t_bres));
+	ray.x = MINI_MAP_CENTER + TILE_SIZE / 2;
+	ray.y = MINI_MAP_CENTER + TILE_SIZE / 2;
+	while (ray_count < WIN_WIDTH)
 	{
 		r = 0;
-		g->is_s = 0;
-		g->is_n = 0;
-		g->is_e = 0;
-		g->is_w = 0;
-		bres.end_x = bres.x + r * cos(g->player.dir + i + g->player.rotate);
-		bres.end_y = bres.y + r * sin(g->player.dir + i + g->player.rotate);
-		while (g->map[(vector.y + (int)bres.end_y - (int)bres.y) / TILE_SIZE]
-			[(vector.x + (int)bres.end_x - (int)bres.x) / TILE_SIZE] != '1')
+		ray.end_x = ray.x + r * cos(g->player.dir + i + g->player.rotate);
+		ray.end_y = ray.y + r * sin(g->player.dir + i + g->player.rotate);
+		while (g->map[(vector.y + (int)ray.end_y - (int)ray.y) / TILE_SIZE]
+			[(vector.x + (int)ray.end_x - (int)ray.x) / TILE_SIZE] != '1')
 		{
-			bres.end_x = bres.x + r * cos(g->player.dir + i + g->player.rotate);
-			bres.end_y = bres.y + r * sin(g->player.dir + i + g->player.rotate);
+			ray.end_x = ray.x + r * cos(g->player.dir + i + g->player.rotate);
+			ray.end_y = ray.y + r * sin(g->player.dir + i + g->player.rotate);
 			r += 0.1;
 		}
-		// if (g->map[(vector.y + (int)bres.end_y - 1 - (int)bres.y) / TILE_SIZE]
-		// 	[(vector.x + (int)bres.end_x - (int)bres.x) / TILE_SIZE] == '1')
-		// 	bres.end_y = round(bres.end_y);
-		// 	// g->is_s = 1;
-		// else
-		// 	bres.end_y = (int)(bres.end_y);
-		// // 	g->is_n = 1;
-		// if (g->map[(vector.y + (int)bres.end_y - (int)bres.y) / TILE_SIZE]
-		// 	[(vector.x + (int)bres.end_x - 1 - (int)bres.x) / TILE_SIZE] == '1')
-		// 	bres.end_x = round(bres.end_x);
-		// 	// g->is_e = 1;
-		// else
-		// 	bres.end_x = (int)(bres.end_x);
-		// 	// g->is_w = 1;
-		// bres.end_y = round(bres.end_y);
-		// bres.end_x = round(bres.end_x);
-		draw_walls(r, i, pixel, g, bres, vector);
-		write_line_bres(img, bres, FOV_BEIGE);
-		// my_mlx_pixel_put(&g->mini_map, bres.end_x - 1, bres.end_y - 1, PLAYER_RED);
+		draw_walls(r, i, ray_count, g, ray);
+		ray.end_y = round(ray.end_y);
+		ray.end_x = round(ray.end_x);
+		write_line_bres(img, ray, FOV_BEIGE);
 		i += FOV_ANGLE * 2 / WIN_WIDTH;
-		pixel++;
+		ray_count++;
 	}
 }
 
@@ -167,9 +140,9 @@ void	draw_player(t_game *g, t_img img)
 {
 	float	i;
 	float	r;
-	t_bres	bres;
+	t_bres	ray;
 
-	ft_bzero(&bres, sizeof(t_bres));
+	ft_bzero(&ray, sizeof(t_bres));
 	if (g->map[g->player.y][g->player.x] == 'N')
 		g->player.dir = -DEGREES_90;
 	if (g->map[g->player.y][g->player.x] == 'S')
@@ -178,19 +151,17 @@ void	draw_player(t_game *g, t_img img)
 		g->player.dir = DEGREES_0;
 	if (g->map[g->player.y][g->player.x] == 'W')
 		g->player.dir = DEGREES_180;
-	bres.x = MINI_MAP_CENTER + TILE_SIZE / 2;
-	bres.y = MINI_MAP_CENTER + TILE_SIZE / 2;
+	ray.x = MINI_MAP_CENTER + TILE_SIZE / 2;
+	ray.y = MINI_MAP_CENTER + TILE_SIZE / 2;
 	i = 0;
 	r = PLAYER_RADIUS;
 	while (i < DEGREES_360)
 	{
-		bres.end_x = round(bres.x + r * cos(i));
-		bres.end_y = round(bres.y + r * sin(i));
-		write_line_bres(img, bres, PLAYER_RED);
+		ray.end_x = round(ray.x + r * cos(i));
+		ray.end_y = round(ray.y + r * sin(i));
+		write_line_bres(img, ray, PLAYER_RED);
 		i += 0.1;
 	}
-	// my_mlx_pixel_put(&img, bres.x, bres.y, 0);
-
 }
 
 void	draw_mini_map_aux(t_img mini_map, t_game *g, t_mini_vector mv)
