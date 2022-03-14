60da5df8f5e3451c33026cb23429df20de0d204d
durantecode <durantecode@gmail.com>
2022-03-14 10:49:18 +0100

    still trying

--- srcs/utils/utils2.c
+++ srcs/utils/utils2.c
@@ -6,7 +6,7 @@
 /*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 14:56:06 by ldurante          #+#    #+#             */
-/*   Updated: 2022/03/08 18:25:25 by ldurante         ###   ########.fr       */
+/*   Updated: 2022/03/14 10:45:13 by ldurante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_vector	get_map_vector(t_game *g, float x, float y)
 {
 	t_vector	vector;
 
-	vector.x = (g->player.x * TILE_SIZE) + round(x + PLAYER_RADIUS);
-	vector.y = (g->player.y * TILE_SIZE) + round(y + PLAYER_RADIUS);
+	vector.x = (g->player.x * TILE_SIZE) + floor(x + PLAYER_RADIUS);
+	vector.y = (g->player.y * TILE_SIZE) + floor(y + PLAYER_RADIUS);
 	return (vector);
 }
